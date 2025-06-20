560bacbeb57d5ec835d8cd4504a3bb357a6e4b3c
Pelin Almalı <148381211+almalipelin@users.noreply.github.com>
2025-06-20 22:21:53 +0300

    Editing scripts

---
+++ Assets/Scripts/PongGame/BounceHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceHandler : MonoBehaviour
+{
+    private Ball ball;
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    Vector3 lastVelocity;
+
+    private void Awake()
+    {
+        ball = GetComponent<Ball>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Wall") || collision.collider.CompareTag("Paddle"))
+        {
+            var speedTwo = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+            rb.velocity = direction * speed;
+        }
+        else if (collision.collider.CompareTag("LeftGoal"))
+        {
+
+        }
+        else if (collision.collider.CompareTag("RightGoal"))
+        {
+
+        }
+    }
+}
