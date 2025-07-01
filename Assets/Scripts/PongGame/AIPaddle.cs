05c96bfa657030f8bc07de439727c2cbde933ebb
Pelin Almalı <148381211+almalipelin@users.noreply.github.com>
2025-07-01 22:00:12 +0300

    update for the pong game

--- Assets/Scripts/PongGame/AIPaddle.cs
+++ Assets/Scripts/PongGame/AIPaddle.cs
@@ -19,17 +19,17 @@ public class AIPaddle : MonoBehaviour
         if(SelectDifficultyController.selectedDifficulty == "Easy")
         {
             moveSpeed = 5f;
-            difficultyOffset = 2f;
+            difficultyOffset = 5f;
         }
         if(SelectDifficultyController.selectedDifficulty == "Medium")
         {
             moveSpeed = 7f;
-            difficultyOffset = 1f;
+            difficultyOffset = 2.5f;
         }
         if(SelectDifficultyController.selectedDifficulty == "Hard")
         {
             moveSpeed = 10f;
-            difficultyOffset = 0.2f;
+            difficultyOffset = 1f;
         }
     }
 
@@ -42,8 +42,12 @@ public class AIPaddle : MonoBehaviour
         }
 
         Vector3 currentPosition = transform.position;
-        float targetY =Ball.position.y;
-        float lerpedY = Mathf.Lerp(currentPosition.y, targetY, moveSpeed*Time.deltaTime/difficultyOffset);
-        transform.position = new Vector3(currentPosition.x, lerpedY, currentPosition.z);
+        Vector3 targetPosition = new Vector3(currentPosition.x, Ball.position.y, currentPosition.z);
+
+        // Sadece moveSpeed'e göre hareket
+        transform.position = Vector3.MoveTowards(currentPosition, targetPosition, moveSpeed * Time.deltaTime);
+        //float targetY =Ball.position.y;
+        //float lerpedY = Mathf.Lerp(currentPosition.y, targetY, moveSpeed*Time.deltaTime/difficultyOffset);
+        //transform.position = new Vector3(currentPosition.x, lerpedY, currentPosition.z);
     }
 }
